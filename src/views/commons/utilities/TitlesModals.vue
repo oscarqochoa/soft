86654b230b9f8245fc347ea76e9663544a2edfd9
luke_16 <lucas.moreno@alarcongroup.us>
2021-12-26 19:13:04 -0500

    aa

--- src/views/commons/utilities/TitlesModals.vue
+++ src/views/commons/utilities/TitlesModals.vue
@@ -1,10 +1,16 @@
 <template>
-  <b-form-group label-class="font-weight-bolder font-small-5" :label="labelTitle">
-    <b-alert variant="info" show>
+  <b-form-group
+    label-class="font-weight-bolder font-small-5"
+    :label="labelTitle"
+  >
+    <b-alert
+      variant="info"
+      show
+    >
       <div
         class="alert-body alert-body bg-info rounded text-white font-medium-1 text-center box-shadow-info"
       >
-        <span>{{title}}</span>
+        <span>{{ title }}</span>
       </div>
     </b-alert>
   </b-form-group>
@@ -12,18 +18,18 @@
 
 <script>
 export default {
-  name: "TitlesModal",
+  name: 'TitlesModal',
   props: {
     title: {
       type: String,
-      default: ""
+      default: '',
     },
     labelTitle: {
       type: String,
-      default: "Title"
-    }
-  }
-};
+      default: 'Title',
+    },
+  },
+}
 </script>
 
 <style >
