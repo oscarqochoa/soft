2f5357b0a3a7c9539f599fdefb700b5c0b7871b5
franco <franco.gallegos@alarcongroup.us>
2022-01-10 19:37:40 -0500

    changing bugs

--- src/views/commons/components/ncr/components/ContentReturned.vue
+++ src/views/commons/components/ncr/components/ContentReturned.vue
@@ -23,7 +23,7 @@
         table-class="text-nowrap"
         responsive="sm"
         show-empty
-        sticky-header="50vh"
+        sticky-header="70vh"
         :current-page="paginate.currentPage"
         :per-page="paginate.perPage"
       >
