5b77a53471fb09baf3c3df2f315236b257bdc246
luke_16 <lucas.moreno@alarcongroup.us>
2022-01-12 16:20:47 -0500

    modal notes all 50%

--- src/views/commons/components/first-notes/ModalNotesAll.vue
+++ src/views/commons/components/first-notes/ModalNotesAll.vue
@@ -15,7 +15,43 @@
           @close="hideModal(false)"
         />
       </template>
-
+      <b-container>
+        <b-row>
+          <b-col
+            v-for="(question, index) in notesSales"
+            :key="index"
+            :md="question.column"
+          >
+            <pre> {{ question }}</pre>
+            <b-form-group
+              v-if="findFather(question.father)"
+              :label="question.question"
+            >
+              <b-form-input
+                v-if="question.type == 2"
+                :ref="'required'+question.question_id"
+                v-model="question.answer"
+                :name="'answer'+question.question_id"
+                @keyup="deleteRequired(question)"
+              />
+              <quill-editor
+                v-else-if="question.type == 0"
+                :id="'required'+question.question_id"
+                v-model="question.answer"
+                :name="'answer'+question.question_id"
+                :disabled="valorEdit"
+                :options="{modules: { toolbar: false },}"
+                @keyup="deleteRequired(question)"
+              />
+              <b-form-radio-group
+                v-else
+                v-model="question.answer"
+                :options="JSON.parse(question.options)"
+              />
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-container>
       <!-- <template #modal-footer>
         <template v-if="newNote">
           <button-save v-if="showButtonSave" @click="saveNotesIncomplete" />
@@ -35,35 +71,36 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { quillEditor } from "vue-quill-editor";
-import vSelect from "vue-select";
-import NotesServices from "@/views/commons/components/first-notes/services/notes.service";
-import HeaderModalNotes from "./HeaderModalNotes.vue";
-import GlobalService from "@/views/services/global.service";
+import { mapGetters } from 'vuex'
+import { quillEditor } from 'vue-quill-editor'
+import vSelect from 'vue-select'
+import NotesServices from '@/views/commons/components/first-notes/services/notes.service'
+import HeaderModalNotes from './HeaderModalNotes.vue'
+import GlobalService from '@/views/services/global.service'
 
-import "quill/dist/quill.core.css";
-import "quill/dist/quill.snow.css";
-import "quill/dist/quill.bubble.css";
-import ButtonSave from "@/views/commons/utilities/ButtonSave";
-import ButtonSaveAndComplete from "@/views/commons/utilities/ButtonSaveAndComplete";
-import ButtonUpdate from "@/views/commons/utilities/ButtonUpdate";
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import ButtonSave from '@/views/commons/utilities/ButtonSave'
+import ButtonSaveAndComplete from '@/views/commons/utilities/ButtonSaveAndComplete'
+import ButtonUpdate from '@/views/commons/utilities/ButtonUpdate'
+
 export default {
-  name: "ModalNotesAll",
+  name: 'ModalNotesAll',
   components: {
     ButtonUpdate,
     ButtonSaveAndComplete,
     ButtonSave,
     vSelect,
     HeaderModalNotes,
-    quillEditor
+    quillEditor,
   },
   props: {
     noteInfo: {
       type: Object,
       required: true,
       default: () => ({
-        programSelected: "",
+        programSelected: '',
         roleId: null,
         notesProgram: null,
         nameProgram: null,
@@ -83,9 +120,9 @@ export default {
         sellerName: null,
         trackings: null,
         notes_status: null,
-        notes_status_new: null
-      })
-    }
+        notes_status_new: null,
+      }),
+    },
   },
   data() {
     return {
@@ -104,8 +141,13 @@ export default {
       origin_country: null,
       countrys: [],
       modalUp: false,
-      notesSales: []
-    };
+      notesSales: [],
+    }
+  },
+  computed: {
+    showContactSchedule() {
+      return this.notesSales[0]
+    },
   },
   mounted() {
     /*  // Filter by date for new fields
@@ -129,238 +171,236 @@ export default {
     this.cleanNotes() */
   },
   created() {
-    this.getNotesSales();
-    this.getCountrys();
-    this.getOriginCountry();
-    console.log(this.noteInfo);
-  },
-  computed: {
-    showContactSchedule() {
-      return this.notesSales[0];
-    }
+    this.getNotesSales()
+    this.getCountrys()
+    this.getOriginCountry()
+    console.log(this.noteInfo)
   },
   methods: {
     async getNotesSales() {
       try {
-        const params = { id: this.noteInfo.saleId };
-        const response = await NotesServices.notesSales(params);
-        this.notesSales = response;
-        this.modalUp = true;
-        this.removePreloader();
+        const params = { id: this.noteInfo.saleId }
+        const response = await NotesServices.notesSales(params)
+        this.notesSales = response
+        this.modalUp = true
+        this.removePreloader()
       } catch (e) {
-        this.showErrorSwal(error);
-        this.removePreloader();
+        this.showErrorSwal(e)
+        this.removePreloader()
       }
     },
     async getCountrys() {
       try {
-        const response = await GlobalService.getCountrys();
-        this.countryOptions = response;
+        const response = await GlobalService.getCountrys()
+        this.countryOptions = response
       } catch (error) {
-        this.showErrorSwal();
+        this.showErrorSwal()
       }
     },
     getOriginCountry() {
-      this.note.originCountry = this.noteInfo.originCountry;
+      this.note.originCountry = this.noteInfo.originCountry
     },
     // Hide Modal
     hideModal(status) {
-      this.modalUp = false;
-      this.$emit("hide", status);
+      this.modalUp = false
+      this.$emit('hide', status)
     },
     closeModal() {
-      this.$emit("close", false);
+      this.$emit('close', false)
     },
     deleteRequired(nota) {
       if (nota.answer != null || nota.answer != null) {
         this.$refs[`required${nota.question_id}`].$el.classList.remove(
-          "required-text"
-        );
+          'required-text',
+        )
       }
     },
-    findfather(id) {
+    findFather(id) {
       if (id != null) {
-        const father = this.notas.find(element => element.question_id == id);
-        return father.answer == "YES";
+        const father = this.notesSales.find(element => element.question_id == id)
+        return father.answer === 'YES'
       }
-      return true;
+      return true
     },
     saveNotesCompleted() {
-      const notes = [];
-      this.addPreloader();
-      let fr = true;
-      let nr = 0;
+      const notes = []
+      this.addPreloader()
+      let fr = true
+      let nr = 0
       for (let x = 0; x < this.notas.length; x++) {
-        if (!this.findfather(this.notas[x].father)) {
-          fr = false;
-          nr += 1;
-          this.notas[x].answer = null;
+        if (!this.findFather(this.notas[x].father)) {
+          fr = false
+          nr += 1
+          this.notas[x].answer = null
         } else {
-          fr = true;
+          fr = true
         }
 
         if (
-          fr == true &&
-          (this.notas[x].answer == null || this.notas[x].answer == "")
+          fr == true
+          && (this.notas[x].answer == null || this.notas[x].answer == '')
         ) {
           this.$refs[`required${this.notas[x].question_id}`].$el.classList.add(
-            "required-text"
-          );
+            'required-text',
+          )
           setTimeout(() => {
-            this.removePreloader();
-          }, 50);
+            this.removePreloader()
+          }, 50)
         }
       }
       this.notas.map(index => {
-        if (index.answer != null && index.answer != "") {
-          notes.push(index);
+        if (index.answer != null && index.answer != '') {
+          notes.push(index)
         }
-      });
+      })
       // Validation for Debt Solution
-      if (this.notas[0].type_view == 2 && this.created >= "2021-06-15") {
-        if (this.contact_schedule == "" || this.contact_schedule == null) {
-          this.contact_error = true;
-          var boton = document.getElementById("loading");
-          boton.classList.remove("preloader");
-          boton.classList.remove("modal-preloader");
+      if (this.notas[0].type_view == 2 && this.created >= '2021-06-15') {
+        if (this.contact_schedule == '' || this.contact_schedule == null) {
+          this.contact_error = true
+          var boton = document.getElementById('loading')
+          boton.classList.remove('preloader')
+          boton.classList.remove('modal-preloader')
         } else {
-          this.contact_error = false;
+          this.contact_error = false
         }
       }
 
       if (
-        this.notas.length == notes.length + nr &&
-        this.contact_error == false
+        this.notas.length == notes.length + nr
+        && this.contact_error == false
       ) {
         swal
           .fire({
-            title: "Are you sure?",
+            title: 'Are you sure?',
             text: "You won't be able to revert this!",
-            type: "warning",
+            type: 'warning',
             showCancelButton: true,
-            confirmButtonColor: "#ab9220",
-            cancelButtonColor: "#8f9194",
-            confirmButtonText: "Yes"
+            confirmButtonColor: '#ab9220',
+            cancelButtonColor: '#8f9194',
+            confirmButtonText: 'Yes',
           })
           .then(result => {
             if (result.value) {
               axios
-                .post("/api/insertSaleNotes", {
+                .post('/api/insertSaleNotes', {
                   notes,
                   sale_id: this.notas[0].sale_id,
                   contact_schedule: this.contact_schedule,
                   originCountry: this.origin_country,
-                  idLead: this.idLead
+                  idLead: this.idLead,
                 })
                 .then(response => {
                   if (response.status == 200) {
                     setTimeout(() => {
-                      const boton = document.getElementById("loading");
-                      boton.classList.remove("preloader");
-                    }, 50);
-                    boton.classList.remove("modal-preloader");
+                      const boton = document.getElementById('loading')
+                      boton.classList.remove('preloader')
+                    }, 50)
+                    boton.classList.remove('modal-preloader')
                     swal
                       .fire({
-                        type: "success",
-                        title: "Notes Successfull"
+                        type: 'success',
+                        title: 'Notes Successfull',
                       })
                       .then(res => {
                         if (res) {
-                          this.$emit("click", false);
+                          this.$emit('click', false)
                         }
-                      });
+                      })
                   }
-                });
+                })
             }
-          });
+          })
       }
     },
 
     saveNotesIncomplete() {
-      const boton = document.getElementById("loading");
+      const boton = document.getElementById('loading')
 
       swal
         .fire({
-          title: "Are you sure?",
+          title: 'Are you sure?',
           text: "You won't be able to revert this!",
-          type: "warning",
+          type: 'warning',
           showCancelButton: true,
-          confirmButtonColor: "#ab9220",
-          cancelButtonColor: "#8f9194",
-          confirmButtonText: "Yes"
+          confirmButtonColor: '#ab9220',
+          cancelButtonColor: '#8f9194',
+          confirmButtonText: 'Yes',
         })
         .then(result => {
           if (result.value) {
             axios
-              .post("/api/insertSaleNotes", {
+              .post('/api/insertSaleNotes', {
                 notes: this.notas,
                 sale_id: this.notas[0].sale_id,
                 contact_schedule: this.contact_schedule,
                 originCountry: this.origin_country,
-                idLead: this.idLead
+                idLead: this.idLead,
               })
               .then(response => {
                 if (response.status == 200) {
                   setTimeout(() => {
-                    const boton = document.getElementById("loading");
-                    boton.classList.remove("preloader");
-                  }, 50);
-                  boton.classList.remove("modal-preloader");
+                    const boton = document.getElementById('loading')
+                    boton.classList.remove('preloader')
+                  }, 50)
+                  boton.classList.remove('modal-preloader')
                   swal
                     .fire({
-                      type: "success",
-                      title: "Notes Successfull"
+                      type: 'success',
+                      title: 'Notes Successfull',
                     })
                     .then(res => {
                       if (res) {
-                        this.$emit("click", false);
+                        this.$emit('click', false)
                       }
-                    });
+                    })
                 }
-              });
+              })
           }
-        });
+        })
     },
     cleanNotes() {
       for (let x = 0; x < this.notas.length; x++) {
-        if (this.notas[x].answer == "null" || this.notas[x].answer == null) {
-          this.notas[x].answer = "";
+        if (this.notas[x].answer == 'null' || this.notas[x].answer == null) {
+          this.notas[x].answer = ''
 
           // Update o Save
-          this.notesCompleted = false;
+          this.notesCompleted = false
           //
         }
       }
-      if (this.contact_schedule == "") {
-        this.notesCompleted = false;
+      if (this.contact_schedule == '') {
+        this.notesCompleted = false
       }
     },
     filterQuestions() {
       // filter repeat clients
-      let noRepeatQuestions = [];
-      const uniqueQuestion = {};
-      noRepeatQuestions = this.notas;
+      let noRepeatQuestions = []
+      const uniqueQuestion = {}
+      noRepeatQuestions = this.notas
 
       noRepeatQuestions = noRepeatQuestions.filter(current => {
-        const exists = !uniqueQuestion[current.question_id];
-        uniqueQuestion[current.question_id] = true;
-        return exists;
-      });
-      this.notas = noRepeatQuestions;
+        const exists = !uniqueQuestion[current.question_id]
+        uniqueQuestion[current.question_id] = true
+        return exists
+      })
+      this.notas = noRepeatQuestions
     },
     getCountries() {
       axios
-        .post("/api/view-countrys", {})
+        .post('/api/view-countrys', {})
         .then(response => {
-          this.countrys = response.data;
+          this.countrys = response.data
         })
         .catch(err => {
-          console.error(err);
-        });
-    }
-  }
-};
+          console.error(err)
+        })
+    },
+  },
+}
 </script>
 
 <style scoped>
+.quill-editor{
+  height: 100px;
+}
 </style>
