f947b9398ff3c29ec028660b699fbd5fdb6497fc
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2022-04-09 19:35:27 -0500

    dist

---
+++ dist/js/chunk-2d0aad79.4474b40b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aad79"],{1378:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("header-slot"),n("clients-list")],1)},a=[],s=n("5530"),r=n("2f62"),u=n("a389"),l={components:{"clients-list":u["a"]},name:"BankOfFlyers",computed:Object(s["a"])({},Object(r["c"])({currentUser:"auth/currentUser",token:"auth/token"}))},o=l,i=n("2877"),d=Object(i["a"])(o,c,a,!1,null,"4f0c79ac",null);t["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0aad79.4474b40b.js.map
